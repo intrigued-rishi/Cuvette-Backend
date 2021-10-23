dbb99b1c516c1319c1070ee2aca793b74cc4a3f4
Hrishikesh Mallick <rishikeshcrever@gmail.com>
2021-10-23 12:08:52 +0530

    added remote db

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Cuvette',{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+mongoose.connect('mongodb+srv://adminuser:useradmin@charitydb.fivwx.mongodb.net/Cuvette?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
     console.log('Database established');
 }).catch((err)=>{
     console.error("Error in establishing database",err);
